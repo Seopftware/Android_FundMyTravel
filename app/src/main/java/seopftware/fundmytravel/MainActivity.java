2e053b4bb66396c2316868fbd446fc9190a26834
Seopftware <inseop0813@gmail.com>
2017-12-22 11:00:13 +0900

    17.12.22_개발 1주차
    첫 번째 Commit
    
    -프로젝트 시작 하기 전 깃헙 연동
    
    *주요기능
    1.방송스트리밍

---
+++ app/src/main/java/seopftware/fundmytravel/MainActivity.java
@@ -0,0 +1,32 @@
+package seopftware.fundmytravel;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+* 인트로 화면
+* @author 김인섭
+* @version 1.0.0
+* @since 2017-12-22 오전 10:53
+* @class comment
+*   이 클래스는 인트로 화면을 표시하기 위한 용도로 만들어졌습니다.
+**/
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private String asd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void ssd() {
+        //ㅁㄴㅇ
+
+
+    }
+}
